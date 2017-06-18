8396fbb8a90a1841abd96643d1e84672a60ce28d
Dotunj <dotunjolaosho@gmail.com>
2017-06-18 22:11:03 +0100

    more commit

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name ="preod";
+
+//Establish database connection 
+
+ $connect = mysqli_connect($servername, $username, $password, $db_name);
+ if(!$connect)
+ 	die("Connection failed: " .mysqli_connect_error());
+} else {
+	echo "Connected to Database successfully";
+}
+
+?>
